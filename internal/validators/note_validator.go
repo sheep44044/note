bfb98df6c040e5ed04d44da5ec67e302cdae600d
WangZhonghao <3290120053@qq.com>
2025-12-02 21:20:31 +0800

    给note请求结构体添加了IsPrivate字段

--- internal/validators/note_validator.go
+++ internal/validators/note_validator.go
@@ -1,13 +1,15 @@
 package validators
 
 type CreateNoteRequest struct {
-	Title   string `json:"title" binding:"required"`
-	Content string `json:"content" binding:"required"`
-	TagIDs  []uint `json:"tag_ids"`
+	Title     string `json:"title" binding:"required"`
+	Content   string `json:"content" binding:"required"`
+	IsPrivate bool   `json:"isPrivate"`
+	TagIDs    []uint `json:"tag_ids"`
 }
 
 type UpdateNoteRequest struct {
-	Title   string `json:"title" binding:"required"`
-	Content string `json:"content" binding:"required"`
-	TagIDs  []uint `json:"tag_ids"`
+	Title     string `json:"title" binding:"required"`
+	Content   string `json:"content" binding:"required"`
+	IsPrivate bool   `json:"isPrivate"`
+	TagIDs    []uint `json:"tag_ids"`
 }
