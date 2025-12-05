f904144e9979de23867ee6d97d2f3eb9485fcc35
WangZhonghao <3290120053@qq.com>
2025-12-05 17:35:49 +0800

    增加UpdateProfileRequest用于个人主页获取相关请求

--- internal/validators/user_validator.go
+++ internal/validators/user_validator.go
@@ -9,3 +9,9 @@ type LoginUserRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+type UpdateProfileRequest struct {
+	Username *string `json:"username,omitempty" binding:"omitempty,min=2,max=20"`
+	Avatar   *string `json:"avatar,omitempty" binding:"omitempty,url"`
+	Bio      *string `json:"bio,omitempty" binding:"omitempty,max=150"`
+}
