e89ec7136c76a18384cc20b0ac2038cb2f76c4f7
WangZhonghao <3290120053@qq.com>
2025-12-05 17:38:34 +0800

    UpdateNoteRequest相关的类型修改为为指针用于分别更新(不是一次更新全部)

--- internal/validators/note_validator.go
+++ internal/validators/note_validator.go
@@ -8,8 +8,8 @@ type CreateNoteRequest struct {
 }
 
 type UpdateNoteRequest struct {
-	Title     string `json:"title" binding:"required"`
-	Content   string `json:"content" binding:"required"`
-	IsPrivate bool   `json:"isPrivate"`
-	TagIDs    []uint `json:"tag_ids"`
+	Title     *string `json:"title" binding:"required"`
+	Content   *string `json:"content" binding:"required"`
+	IsPrivate *bool   `json:"isPrivate"`
+	TagIDs    []uint  `json:"tag_ids"`
 }
